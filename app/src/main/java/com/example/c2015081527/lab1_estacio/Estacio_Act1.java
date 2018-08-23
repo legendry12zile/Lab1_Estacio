cc6aa462303c81bdd1874d7791d9c6f38c8562b4
legendary12zile <2015081527@ust-ics.mygbiz.com>
2018-08-23 13:39:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/c2015081527/lab1_estacio/Estacio_Act1.java
@@ -0,0 +1,13 @@
+package com.example.c2015081527.lab1_estacio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Estacio_Act1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estacio__act1);
+    }
+}
